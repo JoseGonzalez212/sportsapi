73d3b8104f8e750d334bee01eeb45b619428c5d8
Jose Gonzalez <joegonzo990@gmail.com>
2020-11-16 11:38:05 -0600

    Fix filter for games and add more pages

--- views/game.js
+++ views/game.js
@@ -1,8 +1,12 @@
 // This sends a get request for the list of teams
 // It will then create the HTML to display the table
 // Lastly it will append the HTML onto the app
+
+let allGames;
+let allGamesMaster;
+let currentPlaceInGames = 25;
+
 function loadGames() {
-  console.log("test")
   const Http = new XMLHttpRequest();
   const url='http://localhost:3000/games';
   Http.open("GET", url); 
@@ -42,6 +46,11 @@ function loadGames() {
   Http.onreadystatechange = function() {
     if (this.readyState==4 && this.status ==200) {
       let data = JSON.parse(Http.response);
+      allGamesMaster = data;
+      allGames = []
+      for (let i = 0; i < allGamesMaster.length; i++) {
+        allGames.push(allGamesMaster[i])
+      }
       let div = document.createElement("div");
 
       let table = `
@@ -71,7 +80,7 @@ function loadGames() {
         </div>
         </div>
       </div>
-
+      <div id="gamesList">
         <table class=\"table teamtable gameTable\">
           <thead>
             <tr>
@@ -84,9 +93,8 @@ function loadGames() {
           <tbody id="gameRows">
       `;
       
-      for (let i = 0; i < 500; i++) {
+      for (let i = 0; i < 25; i++) {
         let winner;
-        console.log(data[i])
         if (data[i].WL == 'W') {
           winner = data[i].Team
         } else {
@@ -105,6 +113,17 @@ function loadGames() {
       table += `
           </tbody>
         </table>
+        </div>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-center">
+            <li class="page-item ">
+              <a class="page-link" onclick="loadPrevGames(this)" href="#">Previous</a>
+            </li>
+            <li class="page-item">
+              <a class="page-link" onclick="loadNextGames(this)" href="#">Next</a>
+            </li>
+          </ul>
+        </nav>
       `;
       div.innerHTML = table;
 
@@ -116,34 +135,201 @@ function loadGames() {
 
 function filterList(event) {
   let filterYear = event.innerHTML.substring(2, 4);
+  // filter allGames array
+  allGames = []
+  for (let i = 0; i < allGamesMaster.length; i++) {
 
-  let gametable = document.getElementById("gameRows");
-  let gameRows = gametable.getElementsByTagName("tr")
-  for (let i = 0; i < gameRows.length; i++) {
-    let columns = gameRows[i].innerHTML + "";
-    let year = columns.split("<td")[2].split("/")[2].slice(0, -1)
-    if (filterYear == year) {
-      gameRows[i].style.display = ""
+    if (allGamesMaster[i].GameDate !== undefined) {
+      if (allGamesMaster[i].GameDate.split("/")[2] == filterYear)
+        allGames.push(allGamesMaster[i])
+    }
+    
+  }
+
+  //allGames = allGames.filter(game => game.GameDate.split("/")[2] == filterYear)
+  // re create table with this
+  loadGamesFiltered()
+}
+
+// function filterList(event) {
+//   let filterYear = event.innerHTML.substring(2, 4);
+
+//   let gametable = document.getElementById("gameRows");
+//   let gameRows = gametable.getElementsByTagName("tr")
+//   for (let i = 0; i < gameRows.length; i++) {
+//     let columns = gameRows[i].innerHTML + "";
+//     let year = columns.split("<td")[2].split("/")[2].slice(0, -1)
+//     if (filterYear == year) {
+//       gameRows[i].style.display = ""
+//     } else {
+//       gameRows[i].style.display = "none"
+//     }
+//   }
+// }
+
+function loadGamesFiltered() {
+  let table = `
+  <table class=\"table teamtable gameTable\">
+    <thead>
+      <tr>
+        <th scope="col">Match Up</th>
+        <th scope="col">Game Date</th>
+        <th scope="col">Winner</th>
+        <th scope="col">PTS</th>
+      </tr>
+    </thead>
+    <tbody id="gameRows">
+`;
+
+console.log("loadGamesFiltered")
+console.log(allGames)
+currentPlaceInGames = 25;
+let topMax = Math.min(currentPlaceInGames + 25, allGames.length)
+for (let i = currentPlaceInGames; i < topMax; i++) {
+  // console.log(i)
+  // console.log(allGames[i])
+  if (allGames[i] !== undefined) {
+    let winner;
+    if (allGames[i].WL == 'W') {
+      winner = allGames[i].Team
     } else {
-      gameRows[i].style.display = "none"
+      winner = allGames[i].MatchUp.substr(7, 10)
+    }
+    table += `
+      <tr>
+        <td>${allGames[i].MatchUp}</td>
+        <td class="gameDate" >${allGames[i].GameDate}</td>
+        <td>${winner}</td>
+        <td>${allGames[i].PTS}</td>
+      </tr>
+      `
     }
   }
+
+  currentPlaceInGames += 25;
+
+  table += `
+      </tbody>
+    </table>`
+
+  let content = document.getElementById("gamesList")
+  content.innerHTML = table;
 }
+
+// function filterTeam(event) {
+//   let filterTeam = event.innerHTML
+//   let gametable = document.getElementById("gameRows");
+//   let gameRows = gametable.getElementsByTagName("tr")
+//   for (let i = 0; i < gameRows.length; i++) {
+//     let columns = gameRows[i].innerHTML + "";
+//     let year = columns.split("<td")[1]
+//     if (year.includes(filterTeam)) {
+//       gameRows[i].style.display = ""
+//     } else {
+//       if (gameRows[i].style.display != "none")
+//         gameRows[i].style.display = "none"
+//     }
+//   }
+// }
 
 function filterTeam(event) {
   let filterTeam = event.innerHTML
-  console.log(filterTeam)
-  let gametable = document.getElementById("gameRows");
-  let gameRows = gametable.getElementsByTagName("tr")
-  for (let i = 0; i < gameRows.length; i++) {
-    let columns = gameRows[i].innerHTML + "";
-    let year = columns.split("<td")[1]
-    console.log(columns.split("<td")[1])
-    if (year.includes(filterTeam)) {
-      gameRows[i].style.display = ""
-    } else {
-      if (gameRows[i].style.display != "none")
-        gameRows[i].style.display = "none"
-    }
+  // filter allGames array
+  //allGames = []
+  //console.log(allGames[0].Team)
+  let newGames = []
+  for (let i = 0; i < allGames.length; i++) {
+    console.log(allGames[i].Team)
+    if (allGames[i].Team == filterTeam)
+      newGames.push(allGames[i])
   }
+  allGames = newGames
+  //allGames = allGames.filter(game => game.GameDate.split("/")[2] == filterYear)
+  // re create table with this
+  loadGamesFiltered()
+}
+
+function loadNextGames() {
+  let table = `
+  <table class=\"table teamtable gameTable\">
+    <thead>
+      <tr>
+        <th scope="col">Match Up</th>
+        <th scope="col">Game Date</th>
+        <th scope="col">Winner</th>
+        <th scope="col">PTS</th>
+      </tr>
+    </thead>
+    <tbody id="gameRows">
+`;
+
+for (let i = currentPlaceInGames; i < currentPlaceInGames + 25; i++) {
+  let winner;
+  if (allGames[i].WL == 'W') {
+    winner = allGames[i].Team
+  } else {
+    winner = allGames[i].MatchUp.substr(7, 10)
+  }
+  table += `
+    <tr>
+      <td>${allGames[i].MatchUp}</td>
+      <td class="gameDate" >${allGames[i].GameDate}</td>
+      <td>${winner}</td>
+      <td>${allGames[i].PTS}</td>
+    </tr>
+    `
+  }
+
+  currentPlaceInGames += 25;
+
+  table += `
+      </tbody>
+    </table>`
+
+  let content = document.getElementById("gamesList")
+  content.innerHTML = table;
+  
+}
+
+function loadPrevGames() {
+  if (currentPlaceInGames == 25) return 
+  let table = `
+  <table class=\"table teamtable gameTable\">
+    <thead>
+      <tr>
+        <th scope="col">Match Up</th>
+        <th scope="col">Game Date</th>
+        <th scope="col">Winner</th>
+        <th scope="col">PTS</th>
+      </tr>
+    </thead>
+    <tbody id="gameRows">
+`;
+currentPlaceInGames -= 50;
+for (let i = currentPlaceInGames; i < currentPlaceInGames + 25; i++) {
+  let winner;
+  if (allGames[i].WL == 'W') {
+    winner = allGames[i].Team
+  } else {
+    winner = allGames[i].MatchUp.substr(7, 10)
+  }
+  table += `
+    <tr>
+      <td>${allGames[i].MatchUp}</td>
+      <td class="gameDate" >${allGames[i].GameDate}</td>
+      <td>${winner}</td>
+      <td>${allGames[i].PTS}</td>
+    </tr>
+    `
+  }
+
+  currentPlaceInGames += 25;
+
+  table += `
+      </tbody>
+    </table>`
+
+  let content = document.getElementById("gamesList")
+  content.innerHTML = table;
+  
 }
